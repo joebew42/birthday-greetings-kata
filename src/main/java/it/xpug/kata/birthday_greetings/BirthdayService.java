4d3f1c4978eb732946f0f148714816d29f098776
JoeBew42 <joebew42@gmail.com>
2018-05-22 10:41:57 +0200

    inline date parameter

--- src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
+++ src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
@@ -15,14 +15,14 @@ import javax.mail.internet.MimeMessage;
 
 public class BirthdayService {
 
-	public void sendGreetings(String fileName, XDate xDate, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
+	public void sendGreetings(String fileName, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
 		BufferedReader in = new BufferedReader(new FileReader(fileName));
 		String str = "";
 		str = in.readLine(); // skip header
 		while ((str = in.readLine()) != null) {
 			String[] employeeData = str.split(", ");
 			Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
-			if (employee.isBirthday(xDate)) {
+			if (employee.isBirthday(new XDate())) {
 				String recipient = employee.getEmail();
 				String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
 				String subject = "Happy Birthday!";
